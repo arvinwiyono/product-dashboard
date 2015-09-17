eb0644b87cb6a11f9c78d7ec1d7cdab8e25b61f1
Arvin <arvinwiyono@gmail.com>
2015-09-17 16:53:21 +1000

    Add service class

---
+++ app/models/services/product_catalog_api.rb
@@ -0,0 +1,16 @@
+require 'rest-client'
+require 'json'
+
+class ProductCatalogApi
+
+  def initialize
+    @base_url = 'http://localhost:3000/products'
+  end
+
+  def make_request(param = '')
+    url = @base_url + param
+    response = JSON.parse(RestClient.get url)
+    response['products']
+  end
+
+end
