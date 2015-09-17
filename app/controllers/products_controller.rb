838579d5a91eb0d35a11726fc243f5397963b374
Arvin <arvinwiyono@gmail.com>
2015-09-17 16:49:02 +1000

    Refactor code by extracting service to a separate class

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,31 +1,20 @@
-require 'rest-client'
-require 'json'
-
+require_relative '../models/services/product_catalog_api'
 class ProductsController < ApplicationController
 
-
-  @@base_url = 'http://localhost:3000/products'
-
+  @@service = ProductCatalogApi.new()
 
   def all_products
-    @products = make_request
+    @products = @@service.make_request
     render 'main'
   end
 
   def au_products
-    @products = make_request('?region=AU')
+    @products = @@service.make_request('?region=AU')
     render 'main'
   end
 
   def nz_products
-    @products = make_request('?region=NZ')
+    @products = @@service.make_request('?region=NZ')
     render 'main'
-  end
-
-  private
-  def make_request(param = '')
-    url = @@base_url + param
-    response = JSON.parse(RestClient.get url)
-    response['products']
   end
 end
