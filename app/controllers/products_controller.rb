e10ca77e6da698f67dc27e7cd157abd3a3805713
Arvin <arvinwiyono@gmail.com>
2015-09-17 15:08:55 +1000

    Finish HTML + CSS

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,17 +1,31 @@
+require 'rest-client'
+require 'json'
+
 class ProductsController < ApplicationController
 
+
+  @@base_url = 'http://localhost:3000/products'
+
+
   def all_products
-    @products = []
-    render "main"
+    @products = make_request
+    render 'main'
   end
 
   def au_products
-    @products = []
-    render "main"
+    @products = make_request('?region=AU')
+    render 'main'
   end
 
   def nz_products
-    @products = []
-    render "main"
+    @products = make_request('?region=NZ')
+    render 'main'
+  end
+
+  private
+  def make_request(param = '')
+    url = @@base_url + param
+    response = JSON.parse(RestClient.get url)
+    response['products']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'products#all_products'
-  get 'au-products' => 'products#au_products'
-  get 'nz-products' => 'products#nz_products'
+  get 'au_products' => 'products#au_products'
+  get 'nz_products' => 'products#nz_products'
 
 end
